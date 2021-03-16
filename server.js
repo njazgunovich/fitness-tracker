5abcb087e0a81f1da2532fed1b090843f86f069e
njazgunovich <nicholasjaz@gmail.com>
2021-03-16 17:16:29 -0400

    added heroku and atlas

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(() => console.log("Database is Connected"))
+    .catch(err => console.log(err))
 
 // // routes
 app.use(require("./routes/api-routes.js"));
